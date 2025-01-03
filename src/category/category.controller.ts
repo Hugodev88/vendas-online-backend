28c080e9721a10c37a4efdffc70f58967d07c8b3
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-03 14:04:29 -0300

    feat: create categoryService

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReturnCategoryDto } from './dtos/returnCategory.dto';
+import { CategoryService } from './category.service';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
 
+@Roles(UserType.Admin, UserType.User)
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+
+    constructor (
+        private readonly categoryService: CategoryService
+    ) {}
+
+    @Get()
+    async findAllCategories(): Promise<ReturnCategoryDto[]> {
+        return (await this.categoryService.findAllCategories()).map((category) => new ReturnCategoryDto(category))
+    }
+}
