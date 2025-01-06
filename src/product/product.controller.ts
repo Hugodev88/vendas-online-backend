ae7227341cda53a4c87f24169a274562acb085b3
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-06 14:07:09 -0300

    feat: create function delete product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../user/enum/user-type.enum';
 import { ReturnProductDto } from './dtos/returnProduct.dto';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dtos/createProduct.dto';
 import { ProductEntity } from './entities/product.entity';
+import { DeleteResult } from 'typeorm';
 
 @Roles(UserType.Admin, UserType.User)
 @Controller('product')
@@ -24,6 +25,12 @@ export class ProductController {
     @Post()
     async createProduct(@Body() createProduct: CreateProductDto): Promise<ProductEntity> {
         return this.productService.createProduct(createProduct)
+    }
+
+    @Roles(UserType.Admin)
+    @Delete('/:productId')
+    async deleteProduct(@Param('productId') productId: number): Promise<DeleteResult> {
+        return this.productService.deleteProduct(productId)
     }
 
 }
