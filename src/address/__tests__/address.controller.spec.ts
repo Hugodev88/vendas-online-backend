52dd825a1f73f93c3681cfa815069df0525a7591
Hugo Almeida <88250740+Hugodev88@users.noreply.github.com>
2025-01-07 16:12:57 -0300

    feat: create unit test in auth and address controller

---
+++ src/address/__tests__/address.controller.spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AddressController } from '../address.controller';
+import { AddressService } from '../address.service';
+import { createAddressMock } from '../__mocks__/create-address.mock';
+import { userEntityMock } from '../../user/__mocks__/user.mock';
+import { addressMock } from '../__mocks__/address.mock';
+import { ReturnAddressDto } from '../dtos/returnAddress.dto';
+
+describe('AddressController', () => {
+  let controller: AddressController;
+  let addressService: AddressService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AddressService,
+          useValue: {
+            createAddress: jest.fn().mockResolvedValue(addressMock),
+            findAddressByUserId: jest.fn().mockResolvedValue([addressMock]),
+          },
+        }
+      ],
+      controllers: [AddressController],
+    }).compile();
+
+    controller = module.get<AddressController>(AddressController);
+    addressService = module.get<AddressService>(AddressService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(addressService).toBeDefined();
+  });
+
+  it('should return addressEntity in createAddress', async () => {
+    const address = await controller.createAddress(createAddressMock, userEntityMock.id);
+    expect(address).toEqual(addressMock);
+  });
+  
+  it('should return addressEntity in findAddressByUserId', async () => {
+    const addresses = await controller.findAddressByUserId(userEntityMock.id);
+    expect(addresses).toEqual([new ReturnAddressDto(addressMock)]);
+  });
+
+});
